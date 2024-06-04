2659dd412635e100aef252d3063ccc666c14f288
Slavka <arsielka@mail.ru>
2024-06-04 15:11:19 +0500

    fixes all

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+import datetime
+
 from masks import mask_account, mask_card
 
 
@@ -10,12 +12,19 @@ def mask_type(coming_str: str) -> str:
         if i.isalpha():
             mask_number += i + " "
         elif i.isdigit() and len(i) == 16:
-            mask_number += mask_card(i) + " "
+            mask_number += mask_card(i)
         elif i.isdigit() and len(i) == 20:
-            mask_number += mask_account(i) + " "
+            mask_number += mask_account(i)
     return mask_number
 
 
-def date_conv(date: str) -> str:
-    """Функция преобразует дату"""
-    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
+if __name__ == "__main__":
+    result_1 = mask_type("Visa Platinum 7000792289606361")
+    result_2 = mask_type("Счет 73654108430135874305")
+    print(result_1)
+    print(result_2)
+
+
+d1 = datetime.datetime.strptime("2018-07-11T02:26:18.671407", "%Y-%m-%dT%H:%M:%S.%f")
+new_format = d1.strftime("%d-%m-%Y")
+print(new_format)
