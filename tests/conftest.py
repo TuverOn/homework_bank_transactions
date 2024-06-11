58ca565f1a3f7fa64b9d5e32d658249d716eec3a
Slavka <arsielka@mail.ru>
2024-06-11 18:42:56 +0500

    add test_widget

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,8 @@ def card_number():
 @pytest.fixture
 def account_number():
     return "**4305"
+
+
+@pytest.fixture
+def date_for_test():
+    return "11-07-2018"
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,9 +1,9 @@
 import pytest
 
-from src.masks import mask_card, mask_account
+from src.masks import mask_account, mask_card
 
 
-def test_mask_card(card_number):
+def test_mask_card_1(card_number):
     assert mask_card("7000792289606361") == card_number
 
 
@@ -13,10 +13,13 @@ def test_mask_account(account_number):
         mask_account(73654108430135874305)
 
 
-@pytest.mark.parametrize("value, expected", [
-    ("1596837868705199", "1596 83** **** 5199"),
-    ("7158300734726758", "7158 30** **** 6758"),
-    ("6831982476737658", "6831 98** **** 7658"),
-])
-def test_mask_card(value, expected):
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ("1596837868705199", "1596 83** **** 5199"),
+        ("7158300734726758", "7158 30** **** 6758"),
+        ("6831982476737658", "6831 98** **** 7658"),
+    ],
+)
+def test_mask_card_2(value, expected):
     assert mask_card(value) == expected
