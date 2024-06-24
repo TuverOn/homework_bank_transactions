49d3cb41f1cc7818568c5b2a56b820d2bbe60869
Slavka <arsielka@mail.ru>
2024-06-24 13:52:23 +0500

    code test_decorators done

--- src/decorators.py
+++ src/decorators.py
@@ -2,21 +2,23 @@ from functools import wraps
 
 
 def log(filename):
-    '''Декоратор, который будет логировать вызов функции и ее результат в файл или в консоль'''
+    """Декоратор, который будет логировать вызов функции и ее результат в файл или в консоль"""
 
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
-                with open(filename, 'a') as f:
-                    f.write(f'{func.__name__} ok\n')
-                return func(*args, **kwargs)
+                func(*args, **kwargs)
+                log_message = f"{func.__name__} ok"
+                with open(filename, "a") as f:
+                    f.write(log_message + "\n")
+                print(log_message)
 
             except Exception as e:
                 error_message = f"{func.__name__} error: {e}. Inputs:{args}, {kwargs}"
                 with open(filename, "a") as f:
                     f.write(error_message + "\n")
-            return func(*args, **kwargs)
+                print(error_message)
 
         return wrapper
 
@@ -26,5 +28,6 @@ def log(filename):
 @log(filename="mylog.txt")
 def my_function(x, y):
     return x + y
+
 
 my_function(1, "2")
